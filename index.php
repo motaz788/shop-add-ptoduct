1105c20563ecf57a8e4fb5b1168ff530ec6f175c
Moataz Gamal <moatazgamal@MacBook-Pro-alkhas-b-Moataz.local>
2022-05-24 21:40:36 +0200

    adding the pages with shared and organization design first

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php   include './shared/head.php';     
+        include './shared/nav.php' ; 
+ 
+ 
+ 
+ 
+ ?>
+
+<div class="home">
+
+<h1 class="text-center text-info display-1 pt-5">welcome at home page ya bro </h1>
+
+</div>
+
+<?php include './shared/footer.php'; ?>
+
+
+
---
+++ products/update.php
@@ -0,0 +1,28 @@
+<?php   include '../shared/head.php';     
+        include '../shared/nav.php' ; 
+ 
+ 
+ 
+ 
+ ?>
+
+<div class="home">
+
+<h1 class="text-center text-info display-1 pt-5">Update any product </h1>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+<?php include '../shared/footer.php'; ?>
+
+
+
